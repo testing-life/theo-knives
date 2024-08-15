09b824862c931c01c2c04aaa0a6137e0a921b874
M <aethelbrihte@gmail.com>
2024-08-15 19:32:16 +0200

    product styles

--- src/components/molecules/product/product.tsx
+++ src/components/molecules/product/product.tsx
@@ -1,17 +1,25 @@
-import React, { FC } from "react";
-import { storyblokEditable } from "@storyblok/react";
-import ProductGallery from "../productGallery/productGallery";
+import React, { FC } from 'react';
+import { storyblokEditable } from '@storyblok/react';
+import ProductGallery from '../productGallery/productGallery';
+import './product.css';
+
 interface Props {
   blok: any;
 }
 
 const Product: FC<Props> = ({ blok }) => {
+  console.log('blok', blok);
   return (
-    <article {...storyblokEditable(blok)}>
-      product {blok.name}, {blok.model}, {blok.description},{" "}
-      {blok.images[0].filename}
-      {blok.available}
-      <ProductGallery images={["sdf0S"]} />
+    <article {...storyblokEditable(blok)} className='product'>
+      <ProductGallery images={blok.images[0]} />
+      <header className='product__header'>
+        <h2 className='product__name'>{blok.name}</h2>
+        <span className='product__model'>{blok.model}</span>
+      </header>
+      <p className='product__description'>{blok.description}</p>
+      <a className='pure-button theo-button'>
+        {blok.available ? blok.availableLabel : blok.unavailableLabel}
+      </a>
     </article>
   );
 };
