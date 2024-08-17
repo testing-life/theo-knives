c07e5b4633e159ebdead43c593a9bf7dca93a747
M <aethelbrihte@gmail.com>
2024-08-17 20:52:53 +0200

    gallery layout

--- src/components/molecules/product/product.tsx
+++ src/components/molecules/product/product.tsx
@@ -10,24 +10,20 @@ interface Props {
 const Product: FC<Props> = ({ blok }) => {
   console.log('blok', blok);
   return (
-    <article {...storyblokEditable(blok)} className='product'>
+    <article {...storyblokEditable(blok)} className='theo-product'>
       <ProductGallery images={blok.images} />
-      <header className='product__header'>
-        <h2 className='product__name'>{blok.name}</h2>
-        <span className='product__model'>{blok.model}</span>
-      </header>
-      <p className='product__description'>{blok.description}</p>
-      <a className='pure-button theo-button'>
-        {blok.available ? blok.availableLabel : blok.unavailableLabel}
-      </a>
+      <div className='theo-product__content'>
+        <header className='theo-product__header'>
+          <h2 className='theo-product__name'>{blok.name}</h2>
+          <span className='theo-product__model'>{blok.model}</span>
+        </header>
+        <p className='theo-product__description'>{blok.description}</p>
+        <a className='pure-button theo-button'>
+          {blok.available ? blok.availableLabel : blok.unavailableLabel}
+        </a>
+      </div>
     </article>
   );
 };
 
 export default Product;
-
-// name:string
-// model:string
-// description:string
-// images: string[]
-// available: bool
--- src/components/molecules/productGallery/productGallery.tsx
+++ src/components/molecules/productGallery/productGallery.tsx
@@ -1,11 +1,5 @@
 import React, { FC, useState } from 'react';
-import Link from 'components/atoms/link/link';
-import { forDesktop, forTablet } from 'utils/breakpoints';
-import { ReactComponent as Menu } from 'assets/icons/Menu.svg';
-import { ReactComponent as Close } from 'assets/icons/X.svg';
 import './productGallery.css';
-import IconButton from 'components/atoms/iconButton/iconButton';
-import Product from '../product/product';
 
 interface Props {
   images: { [key: string]: string }[];
