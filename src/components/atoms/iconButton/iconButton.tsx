4af6e0130e164234165f5cff92837010cc6c9f7e
M <aethelbrihte@gmail.com>
2024-08-17 21:34:18 +0200

    gallery key support, controls

--- src/components/atoms/iconButton/iconButton.tsx
+++ src/components/atoms/iconButton/iconButton.tsx
@@ -1,5 +1,5 @@
-import React, { FC, ReactNode, ComponentPropsWithoutRef } from "react";
-import "./iconButton.css";
+import React, { FC, ReactNode, ComponentPropsWithoutRef } from 'react';
+import './iconButton.css';
 
 interface Props {
   children: ReactNode;
@@ -20,11 +20,12 @@ const IconButton: FC<Props> = ({
 }) => {
   return (
     <button
+      type='button'
       onClick={clickHandler}
-      {...(needsAria && { "aria-controls": ariaControls })}
-      {...(needsAria && { "aria-haspopup": ariaPopup })}
-      {...(needsAria && { "aria-expanded": ariaExpanded })}
-      className="theo-icon-button"
+      {...(needsAria && { 'aria-controls': ariaControls })}
+      {...(needsAria && { 'aria-haspopup': ariaPopup })}
+      {...(needsAria && { 'aria-expanded': ariaExpanded })}
+      className='theo-icon-button'
     >
       {children}
     </button>
--- src/components/molecules/productGallery/productGallery.tsx
+++ src/components/molecules/productGallery/productGallery.tsx
@@ -1,17 +1,38 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import './productGallery.css';
+import IconButton from 'components/atoms/iconButton/iconButton';
+import { ReactComponent as Prev } from 'assets/icons/Chevron left.svg';
+import { ReactComponent as Next } from 'assets/icons/Chevron right.svg';
+import { forTablet } from 'utils/breakpoints';
 
 interface Props {
   images: { [key: string]: string }[];
 }
 
 const ProductGallery: FC<Props> = ({ images }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const ariaId = 'mainNav';
-  console.log(images);
+  const [slideIndex, setSlideIndex] = useState(0);
+  const sliderRef = useRef<HTMLUListElement>(null);
+
+  const SlideTo = (index: number) => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollTo({
+        left: index * sliderRef.current.clientWidth,
+        behavior: 'smooth',
+      });
+    }
+    setSlideIndex(index);
+  };
+
+  const slidePrev = () => {
+    SlideTo(slideIndex - 1);
+  };
+  const slideNext = () => {
+    SlideTo(slideIndex + 1);
+  };
+
   return (
     <div className='theo-product-gallery'>
-      <ul className='theo-product-gallery__list'>
+      <ul className='theo-product-gallery__list' ref={sliderRef}>
         {images?.map?.((image) => (
           <li className='theo-product-gallery__item'>
             <img
@@ -22,7 +43,19 @@ const ProductGallery: FC<Props> = ({ images }) => {
           </li>
         ))}
       </ul>
-      ;
+      {!forTablet ? (
+        <div className='theo-product-gallery__controls'>
+          <IconButton clickHandler={slidePrev}>
+            <Prev />
+          </IconButton>
+          <span>{`${slideIndex + 1} / ${
+            sliderRef.current?.children.length
+          }`}</span>
+          <IconButton clickHandler={slideNext}>
+            <Next />
+          </IconButton>
+        </div>
+      ) : null}
     </div>
   );
 };
