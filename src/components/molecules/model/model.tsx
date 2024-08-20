ae9c1eede4e8a9040d50b07b7c9ea1a38c25424a
M <aethelbrihte@gmail.com>
2024-08-20 11:16:21 +0200

    model, models list

---
+++ src/components/molecules/model/model.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import { storyblokEditable } from '@storyblok/react';
+import './model.css';
+
+interface Props {
+  blok: any;
+}
+
+const Model: FC<Props> = ({ blok }) => {
+  console.log('blok', blok);
+  return (
+    <article {...storyblokEditable(blok)} className='theo-model'>
+      <figure className='theo-model__figure'>
+        <img className='pure-img' src={blok.image.filename} alt='' />
+      </figure>
+      <h2 className='theo-model__heading'>{blok.name}</h2>
+      <p className='theo-model__description'>{blok.description}</p>
+      <a className='pure-button theo-button theo-model__cta' href={blok.link}>
+        {blok.linkLabel}
+      </a>
+    </article>
+  );
+};
+
+export default Model;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Page from "components/template/Page";
-import Heading from "components/atoms/Heading";
-import Button from "components/atoms/button/button";
-import Link from "components/atoms/link/link";
-import MainNav from "components/molecules/mainNav/MainNav";
-import Product from "components/molecules/product/product";
+import { storyblokInit, apiPlugin } from '@storyblok/react';
+import Page from 'components/template/Page';
+import Heading from 'components/atoms/Heading';
+import Button from 'components/atoms/button/button';
+import Link from 'components/atoms/link/link';
+import MainNav from 'components/molecules/mainNav/MainNav';
+import Product from 'components/molecules/product/product';
+import Model from 'components/molecules/model/model';
+import ModelsList from 'components/molecules/modelsList/modelsList';
 // import Grid from './components/Grid';
 // import Feature from './components/Feature';
 
 storyblokInit({
-  accessToken: "Yg0RlkdoBtQa0vGTYexlmwtt",
+  accessToken: 'Yg0RlkdoBtQa0vGTYexlmwtt',
   use: [apiPlugin],
   components: {
     heading: Heading,
@@ -23,7 +25,9 @@ storyblokInit({
     button: Button,
     link: Link,
     product: Product,
-    "main-nav": MainNav,
+    model: Model,
+    'main-nav': MainNav,
+    models: ModelsList,
     // teaser: Teaser,
     // grid: Grid,
     // feature: Feature,
@@ -31,7 +35,7 @@ storyblokInit({
 });
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
