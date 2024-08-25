26412da4006aea801edab776f75fa291cf1f3193
aethel <aethelbrihte@gmail.com>
2024-08-25 17:03:50 +0200

    model fixes

--- src/components/molecules/model/model.tsx
+++ src/components/molecules/model/model.tsx
@@ -7,6 +7,7 @@ interface Props {
 }
 
 const Model: FC<Props> = ({ blok }) => {
+  console.log('blok', blok);
   const modelType = blok.name?.toLowerCase().split(' ').join('') || 'all';
   return (
     <article {...storyblokEditable(blok)} className='theo-model'>
